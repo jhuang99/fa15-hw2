7915eb12817c7b94143c51438b7417bb1c57bdd8
Jiana Huang <jhuang99@berkeley.edu>
2015-09-25 22:18:16 -0700

    Finished correct

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -1,18 +1,18 @@
-def Person
+class Person
 	def initialize(params = {})
 		@name = params[:name]
 		@age = params[:age]
-		@nickname = name[:3]
+		@nickname = @name.truncate(7)
 	end
 	def introduce()
-		@string = "#{name} #{age}"
-		string
+		@string = "#{@name} #{@age}"
+		@string
 	end
 	def birth_year()
-		@year = (2015 - age.to_i)
-		year
+		@year = (2015 - @age.to_i)
+		@year
 	end
 	def nickname()
-		nickname
+		@nickname
 	end
 end
